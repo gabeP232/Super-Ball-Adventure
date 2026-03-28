965db2cb856a692b3225e7fd80b49e7a203e7e04
Gabe Peregrino <gabe.peregrino00@gmail.com>
2026-03-28 16:37:01 -0700

    Added Settings for All Scenes & Level Switching

---
+++ Assets/Scripts/LevelSwitch.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void LoadLevel1()
+    {
+        LoadLevel("Level1");
+    }
+    public void LoadLevel2()
+    {
+        LoadLevel("Level2");
+    }
+    public void LoadLevel3()
+    {
+        LoadLevel("Level3");
+    }
+
+}
